162f7deedcfb7834b304b2a7b3919affdbde2c07
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 14:50:32 +0100

    Sat Jun 10 14:50:32 BST 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,3 +1,23 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x, y):
+    """
+
+In order to complete the round you need to implement the following method:
+     sum(Integer, Integer) -> Integer
+
+Where:
+ - param[0] = a positive integer between 0-100
+ - param[1] = a positive integer between 0-100
+ - @return = an Integer representing the sum of the two numbers
+
+    Parameters
+    ----------
+    x
+    y
+
+    Returns
+    -------
+
+    """
     raise NotImplementedError()
+
