5187ef45ad66587cba0951c8a8133d3afc3cc583
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 14:56:32 +0100

    Sat Jun 10 14:56:32 BST 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,23 @@
+from typing import Any
+
+
 # noinspection PyShadowingBuiltins,PyUnusedLocal
+
+def _assert_int_in_range_0_100(n: Any):
+    """Check for type and range of.
+    Raises TypeError ValueError
+    Parameters
+    ----------
+    n: Any
+
+    Returns
+    -------
+
+    """
+    if not isinstance(n, int):
+        raise TypeError(f'Type: {type(n)} should by of type {int}')
+
+
 def compute(x: int, y: int) -> int:
     """Returns sum of two positive integers range 0 -> 100.
 
@@ -19,8 +38,10 @@ def compute(x: int, y: int) -> int:
     -------
 
     """
+    _assert_int_int_in_range_0_100(x)
+    _assert_int_int_in_range_0_100(y)
 
-    raise NotImplementedError()
     return x + y
+
 
 
