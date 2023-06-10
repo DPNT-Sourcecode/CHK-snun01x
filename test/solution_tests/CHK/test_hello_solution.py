cd4d4d3fcc27eb9e9223e979aa374131677af7fb
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 17:21:06 +0100

    Sat Jun 10 17:21:06 BST 2023

--- test/solution_tests/CHK/test_hello_solution.py
+++ test/solution_tests/CHK/test_hello_solution.py
@@ -2,10 +2,34 @@ import pytest
 
 from solutions.CHK import checkout_solution
 
+
 class TestCHK:
 
-    def test_validate_skus(self):
-        pass
+    @pytest.mark.parametrize("input_skus", [
+        'a',
+        '1',
+        'abc',
+        'A B C',
+        '123',
+        '',
+    ])
+    def test_validate_skus_invalid(self, input_skus):
+        # ARRANGE
+        # ACT
+        # ASSERT
+        with pytest.raises(TypeError):
+            checkout_solution.validate_skus(input_skus)
+
+    @pytest.mark.parametrize("input_skus", [
+        'AA',
+        'AB'
+    ])
+    def test_validate_skus_valid(self, input_skus):
+        # ARRANGE
+        # ACT
+        # ASSERT
+        checkout_solution.validate_skus(input_skus)
 
     def test_checkout(self):
         pass
+
