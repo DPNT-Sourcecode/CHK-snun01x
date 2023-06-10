58838968af0b3b50a31efa387c7c9164d2077e8b
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 16:48:00 +0100

    Sat Jun 10 16:48:00 BST 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -1,5 +1,15 @@
 # noinspection PyUnusedLocal
 # friend_name = unicode string
 def hello(friend_name):
+    """ No spec.
+    Parameters
+    ----------
+    friend_name
+
+    Returns
+    -------
+
+    """
     return "Not well defined."
+
 
--- test/solution_tests/HLO/hello_solution.py
+++ test/solution_tests/HLO/hello_solution.py
@@ -1,7 +1,9 @@
 import pytest
+from solutions.HLO import hello_solution
 
 
 class TestHLO:
     def test_hello(self):
-        pass
+        hello_solution.hello('string')
+
 
