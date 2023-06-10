138daab9dfd753be4c7791fe6cd93c1b24549117
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 14:53:32 +0100

    Sat Jun 10 14:53:32 BST 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,23 +1,26 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
-def compute(x, y):
-    """
+def compute(x: int, y: int) -> int:
+    """Returns sum of two positive integers range 0 -> 100.
 
-In order to complete the round you need to implement the following method:
-     sum(Integer, Integer) -> Integer
+    In order to complete the round you need to implement the following method:
+         sum(Integer, Integer) -> Integer
 
-Where:
- - param[0] = a positive integer between 0-100
- - param[1] = a positive integer between 0-100
- - @return = an Integer representing the sum of the two numbers
+    Where:
+     - param[0] = a positive integer between 0-100
+     - param[1] = a positive integer between 0-100
+     - @return = an Integer representing the sum of the two numbers
 
     Parameters
     ----------
-    x
-    y
+    x: int 0->100
+    y: int 0->100
 
     Returns
     -------
 
     """
+
     raise NotImplementedError()
+    return x + y
+
 
