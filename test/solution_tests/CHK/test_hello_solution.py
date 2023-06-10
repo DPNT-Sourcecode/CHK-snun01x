42ec96fa03edf1a6e6326be076e55454a0a16644
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 21:30:56 +0100

    Sat Jun 10 21:30:56 BST 2023

--- test/solution_tests/CHK/test_hello_solution.py
+++ test/solution_tests/CHK/test_hello_solution.py
@@ -12,6 +12,56 @@ def _get_sku_parametrization():
         ],
     )
 
+class TestItem:
+
+    def test_item_initialization(self):
+        item = checkout_solution.Item('A', 50, 3)
+        assert item.key == 'A'
+        assert item.price == 50
+        assert item.quantity == 3
+
+    def test_item_total_price(self):
+        item = checkout_solution.Item('A', 50, 3)
+        assert item.total_price == 150
+
+    def test_item_construct(self):
+        item = checkout_solution.Item('A', 50)
+        assert item.quantity == 1
+        item.construct(3)
+        assert item.quantity == 3
+        assert item.total_price == 150
+
+class TestBasket:
+
+    def test_basket_initialization(self):
+        basket = checkout_solution.Basket()
+        assert isinstance(basket.items, dict)
+        assert basket.value == 0
+
+    def test_basket_create(self):
+        basket = checkout_solution.Basket()
+        basket.create_basket("AAABBB")
+        assert len(basket.items) == 2
+        assert basket.items["A"].quantity == 3
+        assert basket.items["B"].quantity == 3
+
+    def test_basket_value(self):
+        basket = checkout_solution.Basket()
+        basket.create_basket("AAABBB")
+        assert basket.value == (checkout_solution.Items["A"].value.total_price*3 + Items["B"].value.total_price*3)
+
+    def test_basket_subtraction(self):
+        basket1 = checkout_solution.Basket().create_basket("AAABBB")
+        basket2 = checkout_solution.Basket().create_basket("AAB")
+        basket1 - basket2
+        assert basket1.items["A"].quantity == 1
+        assert basket1.items["B"].quantity == 2
+
+    def test_basket_subtraction_error(self):
+        basket1 = checkout_solution.Basket().create_basket("AAABBB")
+        basket2 = checkout_solution.Basket().create_basket("AABCC")
+        with pytest.raises(TypeError):
+            basket1 - basket2
 
 class TestDiscount:
     @pytest.mark.parametrize(
@@ -100,4 +150,5 @@ class TestCHK:
 
     def test_checkout_err(self):
         assert checkout_solution.checkout("invalid") == -1
+
 
