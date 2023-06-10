19b7fedf90eadefe64cc8a589c2a858c06cc41ec
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 16:29:52 +0100

    Sat Jun 10 16:29:52 BST 2023

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -24,14 +24,6 @@ def _assert_int_in_range_0_100(n: Any):
 def compute(x: int, y: int) -> int:
     """Returns sum of two positive integers range 0 -> 100.
 
-    In order to complete the round you need to implement the following method:
-         sum(Integer, Integer) -> Integer
-
-    Where:
-     - param[0] = a positive integer between 0-100
-     - param[1] = a positive integer between 0-100
-     - @return = an Integer representing the sum of the two numbers
-
     Parameters
     ----------
     x: int 0->100
@@ -44,3 +36,4 @@ def compute(x: int, y: int) -> int:
     for v in (x, y):
         _assert_int_in_range_0_100(v)
     return x + y
+
