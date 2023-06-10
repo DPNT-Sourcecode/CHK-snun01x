fa32ab7ecea2fa8731e8c419ae82d492302e986f
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 17:33:06 +0100

    Sat Jun 10 17:33:06 BST 2023

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -22,21 +22,48 @@ def validate_skus(skus):
 
 def combine_skus_duplicates(skus: str) -> Dict[str, int]:
     """
+    Counts the frequency of each SKU in a given string.
+
+    The function takes a string of SKUs where each SKU is represented by
+    an uppercase letter [A-Z]. It returns a dictionary where the keys are
+    the SKUs and the values are the count of each SKU in the input string.
+
     Parameters
     ----------
-    skus
+    skus : str
+        A string containing the SKUs to be counted. Each SKU should be an uppercase
+        letter [A-Z].
 
     Returns
     -------
+    dict
+        A dictionary where the keys are the unique SKUs from the input string and the
+        values are the count of each SKU.
+        If a SKU is not present in the input string, it will not appear in the output
+        dictionary.
 
+    Examples
+    --------
+    >>> combine_skus_duplicates('AAB')
+    {'A': 2, 'B': 1}
+
+    >>> combine_skus_duplicates('ABC')
+    {'A': 1, 'B': 1, 'C': 1}
     """
     combined = collections.defaultdict(int)
     for sku in skus:
         combined[sku] += 1
     return combined
 
-def compute_sku_counts_with_discounts(sku_counts:Dict[str, int])->int:
+
+def get_modulo_remainder(n: int, k: int):
+    return n % k, n // k
+
+
+def compute_sku_counts_with_discounts(sku_counts: Dict[str, int]) -> int:
     pass
+
+
 def checkout(skus):
     """
 
@@ -65,6 +92,7 @@ def checkout(skus):
     combined = combine_skus_duplicates(skus)
     # discounts
     raise NotImplementedError()
+
 
 
 
