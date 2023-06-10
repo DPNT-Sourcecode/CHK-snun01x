595608580f95555fe4c4ab18320da2644b99a9ef
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 14:43:58 +0100

    Sat Jun 10 14:43:58 BST 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -4,3 +4,6 @@ from solutions.SUM import sum_solution
 class TestSum():
     def test_sum(self):
         assert sum_solution.compute(1, 2) == 3
+
+
+# I'M SORRY CAN'T USE SCREEN RECORDING DUE TO LAG..
