daad1244cc6cd9c812b8edbc4d49fdf82c2e65d7
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 16:51:00 +0100

    Sat Jun 10 16:51:00 BST 2023

--- lib/solutions/HLO/hello_solution.py
+++ lib/solutions/HLO/hello_solution.py
@@ -2,6 +2,11 @@
 # friend_name = unicode string
 def hello(friend_name):
     """ No spec.
+
+    Some requests have failed (2/2). Here are some of them:
+ - {"method":"hello","params":["Craftsman"],"id":"HLO_R1_001"}, expected: "Hello, World!", got: "Not well defined."
+ - {"method":"hello","params":["Mr. X"],"id":"HLO_R1_002"}, expected: "Hello, World!", got: "Not well defined."
+
     Parameters
     ----------
     friend_name
@@ -10,6 +15,7 @@ def hello(friend_name):
     -------
 
     """
-    return "Not well defined."
+    return "Hello, World!"
+
 
 
--- test/solution_tests/HLO/hello_solution.py
+++ test/solution_tests/HLO/hello_solution.py
@@ -4,6 +4,12 @@ from solutions.HLO import hello_solution
 
 class TestHLO:
     def test_hello(self):
-        hello_solution.hello('string')
+        # ARRANGE
+        expected_result = "Hello, World!"
+        # ACT
+        result = hello_solution.hello("some string")
+        # ASSERT
+        assert result == expected_result
+
 
 
