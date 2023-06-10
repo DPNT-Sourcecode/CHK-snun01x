6006667182be1fdeaaad31fa2c0d043c92918605
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2023-06-10 15:02:32 +0100

    Sat Jun 10 15:02:32 BST 2023

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -1,10 +1,26 @@
+from typing import Any, Union
+
+import pytest
+
 from solutions.SUM import sum_solution
 
 
 class TestSum():
-
+    @pytest.mark.parametrize(
+        ('n', 'error'),
+        [
+            (0, None),
+            (100, None),
+            ('str', TypeError),
+            (-100, ValueError),
+        ]
+    )
+    def test__assert_int_in_range_0_100(self, n: Any, error: Union[None, Any]):
+        if error:
+            with pytest.raises(error):
+                sum_solution._assert_int_in_range_0_100(n)
+        sum_solution._assert_int_in_range_0_100(n)
 
     def test_compute(self):
         assert sum_solution.compute(1, 2) == 3
-
 
